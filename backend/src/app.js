4b1eebe9d72e080c875aaabb15465400e71b31bc
Ismail Ashfak <ashfakismail73@gmail.com>
2026-07-24 23:15:24 +0530

    Fix CORS for Netlify

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,7 +25,8 @@ app.use(helmet());
 app.use(cors({
   origin: [
     "http://127.0.0.1:5501",
-    "http://localhost:5501"
+    "http://localhost:5501",
+    "https://luxora-storee.netlify.app"
   ],
   credentials: true
 }));
